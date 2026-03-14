b8fb609edf1149dfe6f3222d261e1c72c5cdd929
mendes <vitorsmendscod@gmail.com>
2026-03-14 19:00:33 -0300

    add visualization option

--- main.py
+++ main.py
@@ -3,20 +3,38 @@ import logging
 
 from config.data_loader import load_config
 from core.polar_client import PolarClient
-from core.websocket_gateway import WebSocketGateway
 
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s] [%(levelname)s] %(message)s",
 )
 
+
 async def main():
+
     config = load_config("config/config.yaml")
+
     polar = PolarClient(config)
+
     await polar.connect()
     await polar.start_stream()
-    gateway = WebSocketGateway(config, polar)
+
+    try:
+        visualization_enabled = config.get("visualization", "enabled")
+    except Exception:
+        visualization_enabled = False
+
+    if visualization_enabled:
+        from core.websocket_gateway_dashboard import WebSocketGatewayDashboard as Gateway
+        logging.info("Visualization dashboard enabled")
+    else:
+        from core.websocket_gateway import WebSocketGateway as Gateway
+        logging.info("Running standard WebSocket gateway")
+
+    gateway = Gateway(config, polar)
+
     await gateway.start()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
